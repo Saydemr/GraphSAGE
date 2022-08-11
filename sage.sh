9f5c299c8e8ee6b9845e6f845ccad266f408725a
Saydemr <37012364+Saydemr@users.noreply.github.com>
2022-08-11 14:10:39 +0300

    Return to default supervised params

--- sage.sh
+++ sage.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 organism="$1"
 
-# SupModels="graphsage_maxpool graphsage_meanpool"
-# for option in 0 1 2 3 4
+SupModels="gcn graphsage_maxpool graphsage_mean graphsage_meanpool graphsage_seq"
+for model in $SupModels
+do
+    for option in 0 1 2 3 4
+    do
+        rm -f example_data/${organism}-feats.npy
+        if [ $option -eq 1 ]; then 
+            cp example_data/${organism}-sl_feats.npy example_data/${organism}-feats.npy
+        fi
+        if [ $option -eq 2 ]; then 
+            cp example_data/${organism}-ge_feats.npy example_data/${organism}-feats.npy
+        fi
+        if [ $option -eq 3 ]; then 
+            cp example_data/${organism}-go_feats.npy example_data/${organism}-feats.npy
+        fi
+        if [ $option -eq 4 ]; then 
+            cp example_data/${organism}-all-feats.npy example_data/${organism}-feats.npy
+        fi
+    
+        my_dir="supervised/org${organism}_id${identity_dim}_opt${option}"
+        mkdir -p ${my_dir}
+        python -m graphsage.supervised_train --train_prefix ./example_data/${organism} --model ${model} --base_log_dir ${my_dir} --identity_dim 64 --gpu 0
+    
+    done
+done
+
+
+# Models="graphsage_maxpool graphsage_meanpool n2v"
+
+# python -m graphsage.utils example_data/${organism}-G.json example_data/${organism}-walks.txt 
+# for option in 4 2 1 0 3
 # do
-#     rm -f example_data/${organism}-feats.npy
+#     rm -f example_data/${organism}-feats.npy 
 #     if [ $option -eq 1 ]; then 
 #         cp example_data/${organism}-sl_feats.npy example_data/${organism}-feats.npy
 #     fi
@@ -18,68 +47,22 @@ organism="$1"
 #         cp example_data/${organism}-all-feats.npy example_data/${organism}-feats.npy
 #     fi
 
-#     for epoch in 50
+#     for identity_dim in 32
 #     do
-#         for batch_size in 32
+#         for lr in 0.0001  
 #         do
-#             for lr in 0.01 0.001 0.0001
+#             for epoch in 1
 #             do
-#                 for validate_iter in 100
+#                 for batch_size in 512
 #                 do
-#                     for validate_batch_size in 512
+#                     for model in $Models
 #                     do
-#                         for identity_dim in 32
-#                         do
-#                             for model in $SupModels
-#                             do
-#                                 my_dir="supervised/org${organism}_e${epoch}_b${batch_size}_v${validate_iter}_vb${validate_batch_size}_id${identity_dim}_opt${option}"
-#                                 mkdir -p ${my_dir}
-#                                 python -m graphsage.supervised_train --train_prefix ./example_data/${organism} --model ${model} --batch_size ${batch_size} --epochs ${epoch} --identity_dim ${identity_dim} --learning_rate ${lr} --validate_iter ${validate_iter} --validate_batch_size ${validate_batch_size} --print_every 10000 --base_log_dir ${my_dir}  --gpu 0
-#                             done
-#                         done
+#                         my_dir="unsupervised/org${organism}_e${epoch}_b${batch_size}_id${identity_dim}_opt${option}"
+#                         mkdir -p ${my_dir}
+#                         python -m graphsage.unsupervised_train --train_prefix ./example_data/${organism} --model ${model} --batch_size ${batch_size} --epochs ${epoch} --identity_dim ${identity_dim} --learning_rate ${lr} --validate_iter 100 --print_every 100 --max_total_steps 1000 --base_log_dir ${my_dir} --gpu 0
 #                     done
 #                 done
 #             done
 #         done
 #     done
 # done
-
-
-Models="graphsage_maxpool graphsage_meanpool n2v"
-
-python -m graphsage.utils example_data/${organism}-G.json example_data/${organism}-walks.txt 
-for option in 0 1 2 3 4
-do
-    rm -f example_data/${organism}-feats.npy 
-    if [ $option -eq 1 ]; then 
-        cp example_data/${organism}-sl_feats.npy example_data/${organism}-feats.npy
-    fi
-    if [ $option -eq 2 ]; then 
-        cp example_data/${organism}-ge_feats.npy example_data/${organism}-feats.npy
-    fi
-    if [ $option -eq 3 ]; then 
-        cp example_data/${organism}-go_feats.npy example_data/${organism}-feats.npy
-    fi
-    if [ $option -eq 4 ]; then 
-        cp example_data/${organism}-all-feats.npy example_data/${organism}-feats.npy
-    fi
-
-    for identity_dim in 32
-    do
-        for lr in 0.0001  
-        do
-            for epoch in 1
-            do
-                for batch_size in 512
-                do
-                    for model in $Models
-                    do
-                        my_dir="unsupervised/org${organism}_e${epoch}_b${batch_size}_id${identity_dim}_opt${option}"
-                        mkdir -p ${my_dir}
-                        python -m graphsage.unsupervised_train --train_prefix ./example_data/${organism} --model ${model} --batch_size ${batch_size} --epochs ${epoch} --identity_dim ${identity_dim} --learning_rate ${lr} --validate_iter 100 --print_every 1000 --max_total_steps 10000 --base_log_dir ${my_dir} --gpu 0
-                    done
-                done
-            done
-        done
-    done
-done
